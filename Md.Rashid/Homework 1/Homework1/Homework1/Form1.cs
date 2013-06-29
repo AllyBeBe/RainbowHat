e6667dbee7898d057e4165db4a1261e0d6192ef1
Ahsan <ahsan.jnl+ms@gmail.com>
2013-06-28 20:18:33 -0700

    hw#1, step #10

--- Md.Rashid/Homework 1/Homework1/Homework1/Form1.cs
+++ Md.Rashid/Homework 1/Homework1/Homework1/Form1.cs
@@ -21,5 +21,10 @@ namespace Homework1
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dot Net Perls is awesome.");
+        }
     }
 }
