52635026cae26c503941d7130bc4ec4fcd6983ad
Scown <bscown@daptiv.com>
2013-07-01 20:12:41 -0700

    Super-Duper Extra Credit

--- Bryan.Scown/Homework1/Homework1/Form1.cs
+++ Bryan.Scown/Homework1/Homework1/Form1.cs
@@ -15,11 +15,36 @@ namespace Homework1
         public Form1()
         {
             InitializeComponent();
+            textBox1.Select();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Seattle Seahawks!");
+            string comments = null;
+            string inputText = textBox1.Text;
+            string answer = "Your Response: ";
+
+            if (textBox1.Text == string.Empty)
+            {
+                inputText = "You didn't enter any text";
+            }
+            if (textBox1.Text.Contains("hawk") || textBox1.Text.Contains("eattle"))
+            {
+                inputText = textBox1.Text;
+                comments = "The Seattle Seahawks are my favorite team too!";
+            }
+            if (textBox1.Text.Contains("ittsburg") || textBox1.Text.Contains("teelers"))
+            {
+                inputText = textBox1.Text;
+                comments = "The Steelers SUCK!!!!!!!";
+            }
+            MessageBox.Show(answer + inputText + "\n" + "\n" + comments);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
