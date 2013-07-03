8ee4af16f3bcdb9500973a9640bfc2cf495e9809
Alex Purice <alexei.purice@gmail.com>
2013-07-03 16:19:56 -0700

    Homework 1 completed
    I'm sorry for missing baseline commit. This commit includes extra and
    super duper extra credit assignments.

---
+++ Alex.Purice/Homework 1/Homework1/Homework1/AlexHomework1ExtraCredit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class AlexHomework1ExtraCredit : Form
+    {
+        public AlexHomework1ExtraCredit()
+        {
+            InitializeComponent();
+        }
+
+        private void showExtraCreditMessage(object sender, EventArgs e)
+        {
+            DialogResult proceedWithTheHomeWork = MessageBox.Show("Thank you for checking extra credit assignment.\n\n\nWould you like to proceed to super-duper extra credit part of the homework?", "Important Question", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+            switch (proceedWithTheHomeWork)
+            {
+                case DialogResult.Yes :
+                    this.openExtaSuperDuperWindow();
+                    break;
+                case DialogResult.No :
+                    extraCreditButton.Hide();
+                    superDuperExtraCreditButton.Location = new Point(199, 102);
+                    superDuperExtraCreditButton.Show();
+                    //
+                    break;
+            }
+        }
+
+        private void superDuperExtraCreditButton_Click(object sender, EventArgs e)
+        {
+            this.openExtaSuperDuperWindow();
+        }
+
+        private void openExtaSuperDuperWindow()
+        {
+            AlexHomework1SuperDuperExtraCreditForm newAlexHomework1SuperDuperExtraCreditForm = new AlexHomework1SuperDuperExtraCreditForm();
+            newAlexHomework1SuperDuperExtraCreditForm.ShowDialog();
+        }
+
+    }
+}
---
+++ Alex.Purice/Homework 1/Homework1/Homework1/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class AlexHomework1SuperDuperExtraCreditForm : Form
+    {
+        public AlexHomework1SuperDuperExtraCreditForm()
+        {
+            InitializeComponent();
+            textSpellInput.MaxLength = 50;
+        }
+
+        private void magicButton_Click(object sender, EventArgs e)
+        {
+            blackArtLabel.Visible = true;
+            blackArtServiceLabel.Visible = true;
+            spellResultLabel.Visible = true;
+            spellResultLabel.Text = textSpellInput.Text;
+        }
+
+        private void textSpellInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textSpellInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            int charactersLeftForLabel;
+            int currentValueLength;
+            int maxTextFieldLength = this.textSpellInput.MaxLength;
+
+            currentValueLength = this.textSpellInput.Text.Length;
+
+            if (currentValueLength == 0)
+            {
+                spellInputLabel.Text = "Enter your spell (50 characters limit per spell):";
+                spellServiceLabel.Visible = false;
+                blackArtLabel.Visible = false;
+                blackArtServiceLabel.Visible = false;
+                spellResultLabel.Visible = false;
+                spellResultLabel.Text = "";
+                
+            }
+            else
+            {
+                charactersLeftForLabel = maxTextFieldLength - currentValueLength;
+                spellInputLabel.Text = "Enter you spell (" + charactersLeftForLabel.ToString() + " characters left):";
+                spellServiceLabel.Visible = true;
+            }
+        }
+
+
+    }
+}
