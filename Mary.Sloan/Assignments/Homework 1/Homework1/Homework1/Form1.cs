a75096d5a18abf9da83e066b42673dbf3decece7
Mary Sloan <M_Hagen@msn.com>
2013-07-03 09:29:53 -0700

    Extra credit - add button and message.

--- Mary.Sloan/Assignments/Homework 1/Homework1/Homework1/Form1.cs
+++ Mary.Sloan/Assignments/Homework 1/Homework1/Homework1/Form1.cs
@@ -16,5 +16,11 @@ namespace Homework1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = "Todo";
+            MessageBox.Show(msg);
+        }
     }
 }
