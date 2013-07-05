cee41dbbde2fb87ff361631b52a0e486b34cce6a
Paul Sheridan <paul.sheridan@me.com>
2013-07-04 21:09:16 -0700

    Homework1 Complete + Super-duper extra credit

--- Paul.Sheridan/Homework 1/Homework1/Homework1/Form1.cs
+++ Paul.Sheridan/Homework 1/Homework1/Homework1/Form1.cs
@@ -16,5 +16,15 @@ namespace Homework1
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Well done.  You typed: " + textBox1.Text + "!");
+        }
     }
 }
