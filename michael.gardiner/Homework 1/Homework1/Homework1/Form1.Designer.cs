6410839a232bb2830ef65fb023b89de5f7082914
Michael Gardiner <migard@uw.edu>
2013-07-05 13:04:08 -0700

    Homework 1 Completed

--- michael.gardiner/Homework 1/Homework1/Homework1/Form1.Designer.cs
+++ michael.gardiner/Homework 1/Homework1/Homework1/Form1.Designer.cs
@@ -34,11 +34,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.Size = new System.Drawing.Size(131, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Change this to get a passing grade.";
+            this.label1.Text = "Hello World!";
             // 
             // Form1
             // 
