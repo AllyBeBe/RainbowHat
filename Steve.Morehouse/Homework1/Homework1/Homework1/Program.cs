a851f5bda16e0ee10fd31ed214623a16e01a19cb
coyotesm <coyotesm@u.washington.edu>
2013-07-05 18:37:12 -0700

    HW1 Completed

--- Steve.Morehouse/Homework1/Homework1/Homework1/Program.cs
+++ Steve.Morehouse/Homework1/Homework1/Homework1/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Steve Morehouse
+ * coyotesm@u.washington.edu
+ */
+
 namespace Homework1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Print Hello World
+            System.Console.WriteLine("Hello World !");
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey(true);
         }
     }
 }
