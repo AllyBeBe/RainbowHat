e9b505d983bbdaa9e466fa33810bf0f9430002de
Jay Canon <jbcanon@uw.edu>
2013-07-09 13:07:27 -0700

    Homework1 Completed

---
+++ jay.canon/Assignments/Homework1/Homework1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Homework1
+{
+    partial class frmHomework1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHW1Label = new System.Windows.Forms.Label();
+            this.lblEnterName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHW1Label
+            // 
+            this.lblHW1Label.AutoSize = true;
+            this.lblHW1Label.Location = new System.Drawing.Point(48, 27);
+            this.lblHW1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHW1Label.Name = "lblHW1Label";
+            this.lblHW1Label.Size = new System.Drawing.Size(296, 17);
+            this.lblHW1Label.TabIndex = 0;
+            this.lblHW1Label.Text = "Changed for Homework 1 - Due July 11, 2013";
+            // 
+            // lblEnterName
+            // 
+            this.lblEnterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterName.Location = new System.Drawing.Point(12, 84);
+            this.lblEnterName.Name = "lblEnterName";
+            this.lblEnterName.Size = new System.Drawing.Size(360, 57);
+            this.lblEnterName.TabIndex = 1;
+            this.lblEnterName.Text = "Please enter your name below and press OK to continue:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(17, 144);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(355, 30);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdOK.Location = new System.Drawing.Point(149, 194);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(75, 45);
+            this.cmdOK.TabIndex = 2;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.okClick);
+            // 
+            // frmHomework1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 266);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblEnterName);
+            this.Controls.Add(this.lblHW1Label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmHomework1";
+            this.Text = "Homework 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHW1Label;
+        private System.Windows.Forms.Label lblEnterName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button cmdOK;
+    }
+}
+
---
+++ jay.canon/Assignments/Homework1/Homework1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class frmHomework1 : Form
+    {
+        public frmHomework1()
+        {
+            InitializeComponent();           
+        }
+
+        private void okClick(object sender, EventArgs e)
+        {
+            MessageBox.Show ("Hello, " + txtUserName.Text + ".  It is nice to meet you." + "\n\nThe current system date & time is:  "+ DateTime.Now);
+            
+        }
+    }
+}
