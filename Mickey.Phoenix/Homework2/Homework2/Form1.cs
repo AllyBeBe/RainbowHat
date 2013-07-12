711fd8fc78eaecdabab5c8fba9e3b34b7b01931b
Mickey Phoenix <meowse@remove.gmail.remove.com>
2013-07-11 21:01:55 -0700

    rest of clicky label

--- Mickey.Phoenix/Homework2/Homework2/Form1.cs
+++ Mickey.Phoenix/Homework2/Homework2/Form1.cs
@@ -16,5 +16,10 @@ namespace Homework2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicky label!");
+        }
     }
 }
