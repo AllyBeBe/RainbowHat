93d022ee2e3ec7bd545f4b90841c510a9dc3ed1c
coyotesm <coyotesm@u.washington.edu>
2013-07-13 20:13:08 -0700

    redo homework1
    originally checked it out as a console main prog, but to use forms, it's
    easier to change this to be a forms app so I can use
    System.Windows.Forms

--- Steve.Morehouse/Homework1/Homework1/Homework1/Program.cs
+++ Steve.Morehouse/Homework1/Homework1/Homework1/Program.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 /*
  * Steve Morehouse
  * coyotesm@u.washington.edu
  */
 
-namespace Homework1
+namespace WindowsFormsApplication1
 {
-    class Program
+    namespace Homework1
     {
-        static void Main(string[] args)
+        class Program
         {
-            //Print Hello World
-            System.Console.WriteLine("Hello World !");
-
-            Console.WriteLine("Press any key to continue...");
-
-            Console.ReadKey(true);
+            static void Main(string[] args)
+            {
+                //Print Hello World
+                System.Console.WriteLine("Hello World !");
+                Console.ReadKey(true);
+                Console.WriteLine("Press any key to continue...");
+            }
         }
     }
 }
