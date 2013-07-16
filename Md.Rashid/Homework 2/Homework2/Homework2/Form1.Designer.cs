49f5ef70b0da2af349399eb60ff211e1b3a9e4fc
Ahsan <ahsan.jnl+ms@gmail.com>
2013-07-16 01:38:33 -0700

    2nd commit
    1. I did extra credit from last HW, so didnt really repeat them
    2. Added a press me button that shows a pop up message box
    3. added a rich text editor that I never used before (super duper extra
    credit)
    4. modified lil code inside form1 designer code.cs (super duper extra
    credit)
    5. did super duper extra credit - have a text box promting for user name
    (with hint) and dispalying name once user inputs name

--- Md.Rashid/Homework 2/Homework2/Homework2/Form1.Designer.cs
+++ Md.Rashid/Homework 2/Homework2/Homework2/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pressButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pressButton
+            // 
+            this.pressButton.Location = new System.Drawing.Point(282, 272);
+            this.pressButton.Name = "pressButton";
+            this.pressButton.Size = new System.Drawing.Size(75, 23);
+            this.pressButton.TabIndex = 0;
+            this.pressButton.Text = "Press Me  !";
+            this.pressButton.UseVisualStyleBackColor = true;
+            this.pressButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pressButton_MouseClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 223);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Your Name ?";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "It\'s ME !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(500, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 13);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 347);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pressButton);
+            this.Name = "Form1";
+            this.Text = "Ahsan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button pressButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Md.Rashid/Homework 2/Homework2/Homework2/Form1.cs
+++ Md.Rashid/Homework 2/Homework2/Homework2/Form1.cs
@@ -16,5 +16,36 @@ namespace Homework2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pressButton.BackColor = Color.Beige;
+
+            textBox1.SelectionStart = 0;
+            textBox1.SelectionLength = 0;
+            textBox1.ForeColor = Color.Gray;
+
+        }
+
+        private void pressButton_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            MessageBox.Show("Dot Net Perls is awesome.","Awesome Message");
+            
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = textBox1.Text.Equals("Your Name ?") ? "" : textBox1.Text;
+            label1.Text = "Hello " + text + "!";
+        }
+
+
     }
 }
