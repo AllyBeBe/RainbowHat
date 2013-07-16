88755554716a226a18de2af67e3534b08eef28fa
Ahsan <ahsan.jnl+ms@gmail.com>
2013-07-16 02:24:08 -0700

    super super super duper extra credit + super duper extra credit
    1. added controls manually in the form designer.cs
    2. did the super super super duper extra credit part of the assignment,
    input prompt for numbers, validation and visual feedback for saving

--- Md.Rashid/Homework 2/Homework2/Homework2/Form1.cs
+++ Md.Rashid/Homework 2/Homework2/Homework2/Form1.cs
@@ -25,6 +25,7 @@ namespace Homework2
             textBox1.SelectionLength = 0;
             textBox1.ForeColor = Color.Gray;
 
+            savedLabel.Visible = false;
         }
 
         private void pressButton_MouseClick(object sender, MouseEventArgs e)
@@ -44,6 +45,40 @@ namespace Homework2
         {
             String text = textBox1.Text.Equals("Your Name ?") ? "" : textBox1.Text;
             label1.Text = "Hello " + text + "!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Validate the number input
+            String text2 = textBox2.Text.Trim();
+            String text3 = textBox3.Text.Trim();
+            String text4 = textBox4.Text.Trim();
+            String text5 = textBox5.Text.Trim();
+
+
+            if (text2.Length == 0 || text3.Length == 0 || text4.Length == 0 || text5.Length == 0) 
+            {
+                MessageBox.Show("Please check your phone number input again, you are missing some numbers","Input Validation Error !");
+            }
+            else if (text2.Length != 1 || text3.Length != 3 || text4.Length != 3 || text5.Length != 4)
+            {
+                MessageBox.Show("Please check your phone number input again, you may have entered more or less digits", "Input Validation Error !");
+            }
+            else
+            {
+                string Str = text2 + text3 + text4 + text5; ;
+                double Num;
+                bool isNum = double.TryParse(Str, out Num);
+                if (isNum)
+                {
+                    savedLabel.Visible = true;
+                }
+                else
+                {
+                    MessageBox.Show("Please check your phone number input again, invalid character(s) detected!", "Input Validation Error !");
+                }
+            }
+
         }
 
 
