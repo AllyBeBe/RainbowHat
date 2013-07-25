d5b54be414c9272db0a1f9c62885571adc3715ce
Evacarlstrom <evacarlstrom@gmail.com>
2013-07-24 23:57:35 -0700

    Homework 3 BooleanLogic partly done
    refactored everything; haven't yet done rental car bit

--- Eva.Carlstrom/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Eva.Carlstrom/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -4,85 +4,60 @@
     {
         public bool NegatesItsInput(bool input)
         {
-            if (input == true)
+            if (input)
             {
                 return false;
             }
-            else
-            {
-                return true;
-            }
+            return true;
         }
 
         public bool NegatesItsInputSingleLine(bool input)
         {
-            // Use ReSharper on the above to reduce it to a single line.
-            return false;
+            // Use resharper on the above to reduce it to a single line.
+            return !input;
         }
 
         public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
         {
-            if (input1 == true)
+            if (input1)
             {
-                if (input2 == true)
+                if (input2)
                 {
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
-            else
             {
-                if (input1 == true)
-                {
-                    return false;
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
         }
 
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 && input2;
+            
         }
 
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
         {
-            // Use resharper on this to reduce it to a single line.
-            if (input1 == true)
-            {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return true;
-                }
+            if (input1)
+            {   
+                return true;         
             }
-            else
+            if (input2)
             {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
+            return false;
         }
 
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
+            return input1 || input2;
+            
             // Use resharper on the above to reduce it to a single line.
-            return false;
+
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
