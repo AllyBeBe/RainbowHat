ffbc6cb7b62243071975ed54bb70a7bdaffe159b
Amy Hurlow <aehurlow@gmail.com>
2013-07-24 19:14:07 -0700

    First attempt Homework3

--- Amy.Hurlow/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Amy.Hurlow/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -4,14 +4,7 @@
     {
         public bool NegatesItsInput(bool input)
         {
-            if (input == true)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+            return !input;
         }
 
         public bool NegatesItsInputSingleLine(bool input)
@@ -22,28 +15,7 @@
 
         public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
         {
-            if (input1 == true)
-            {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                if (input1 == true)
-                {
-                    return false;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+            return input1 && input2;
         }
 
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
@@ -55,28 +27,7 @@
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
         {
             // Use resharper on this to reduce it to a single line.
-            if (input1 == true)
-            {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+            return input1 || input2;
         }
 
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
