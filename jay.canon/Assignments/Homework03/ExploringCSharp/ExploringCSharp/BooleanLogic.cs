b60184f90f8b08c14ff055307aaf163c1e1b469d
Jay Canon <jbcanon@uw.edu>
2013-07-24 18:13:51 -0700

    Homework 03 Core & Extra Credit Completed

--- jay.canon/Assignments/Homework03/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ jay.canon/Assignments/Homework03/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -57,8 +57,7 @@
 
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 == true ? input2 == true : input1 == true && false;
         }
 
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
@@ -90,8 +89,7 @@
 
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 ? input2 == true || true : input2 == true;
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
@@ -99,7 +97,21 @@
             // Implement this one from scratch so that all tests pass.  
             // Age is a whole number.  The intended values and meanings of the string "gender"
             // can be inferred from the tests.
-            return false;
+
+            switch (gender)
+            {
+                case "F": //females of any age do not pay extra rental surcharge
+                    return false;
+                case "M": //males age under 25 pay extra rental surcharge
+                    return age < 25 ? true : false;
+                case "O": //persons of non-traditional gender age under 25 pay extra rental surcharge
+                    return age <= 25 ? true : false;
+                case "P": //persons preferring not to state gender age under 25 pay extra rental surcharge
+                    return age <= 25 ? true : false;
+                default:
+                    return true;
+            }
         }
     }
 }
+ 
--- jay.canon/Assignments/Homework03/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ jay.canon/Assignments/Homework03/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,8 @@
-﻿namespace ExploringCSharp
+﻿using System;
+using System.Diagnostics;
+
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,13 +14,13 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+            return Math.Max(number1, number2);
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
             // Try googling "C# absolute value of a number"
-            return 0;
+            return Math.Abs(number1) >= Math.Abs(number2) ? number1 : number2;
         }
 
         public int MultiplyByTheNextLargerPowerOfTen(int number)
@@ -24,7 +28,20 @@
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+
+            //calculate the Power of Ten value of the passed integer
+            int powerOfTen = (int) (Math.Pow(10, (int) Math.Log10(number)));
+
+
+            if (number == powerOfTen)
+                //if the passed integer is already a Power of Ten Value of itself, 
+                //multiply the Power of Ten value to the passed integer;
+                //also satisfies the condition where passed integer = 1
+            {
+                return number * powerOfTen;
+            }   
+            // else, multiply the passed integer by the next greatest Power of Ten value
+            return number * (powerOfTen * 10);
         }
     }
 }
