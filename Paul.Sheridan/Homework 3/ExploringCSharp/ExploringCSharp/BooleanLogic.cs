98c92ecf818a4bee8d8c59a3338a49a313c30ce5
Paul Sheridan <paul.sheridan@me.com>
2013-07-24 19:41:38 -0700

    BooleanLogic.cs complete with Extra Credit
    For some reason, the extra credit wasn't included in my last commit.
    It's complete this time.

--- Paul.Sheridan/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Paul.Sheridan/Homework 3/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -79,28 +79,7 @@
 
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
-            if (input1)
-            {
-                if (input2)
-                {
-                    return true;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                if (input2)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+            return input1 || input2;
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
@@ -108,7 +87,15 @@
             // Implement this one from scratch so that all tests pass.  
             // Age is a whole number.  The intended values and meanings of the string "gender"
             // can be inferred from the tests.
-            return false;
+            switch (gender)
+            {
+                case "F": // If the driver is female, she pays no extra charge, regardless of age.
+                    return false;
+                case "M": // If the driver is male, he'll pay an extra charge until he's 25. The "=" made the tests fail, so it was removed.
+                    return age < 25 ? true : false;
+                default: // For any other driver, the fee will be paid.  We are a business, after all.
+                    return true;
+            }
         }
     }
 }
