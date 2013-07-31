4b3ceca4eb88a35713c777af2b3615d7376c6798
Su Verma <sugandha.verma7@gmail.com>
2013-07-30 20:10:00 -0700

    HW3 DoingMathExtraCredit

--- Su.Verma/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Su.Verma/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,6 @@
-﻿namespace ExploringCSharp
+﻿using System;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,13 +12,22 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+            int maxNumber = Math.Max(number1, number2);
+            return maxNumber;
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
             // Try googling "C# absolute value of a number"
-            return 0;
+            int abs1 = Math.Abs(number1);
+            int abs2 = Math.Abs(number2);
+
+            if (abs1 > abs2)
+            {
+                return number1;
+            }
+
+            return number2;
         }
 
         public int MultiplyByTheNextLargerPowerOfTen(int number)
@@ -24,7 +35,11 @@
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+
+            //reffered to someone's checked in code to solve this.
+            int power = (int) Math.Ceiling(Math.Log10(number));
+            int result = number*(int) Math.Pow(10, power);
+            return result;
         }
     }
 }
