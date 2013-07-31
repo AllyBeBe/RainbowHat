f797763d9552914d4b36cc7e42d5042413d45c06
Mickey Phoenix <meowse@remove.gmail.remove.com>
2013-07-31 13:35:22 -0700

    Code samples from Office Hours 4.5a.
    Relates to making tests pass, to the structure of the if-statement, and
    to the meaning and appropriate use of curly-braces.

--- Mickey.Phoenix/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Mickey.Phoenix/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,16 +1,104 @@
-﻿namespace ExploringCSharp
+﻿using System;
+using System.Diagnostics;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
         public int ReturnTheLargerNumber(int number1, int number2)
         {
+            // Pseudocode (informal, i.e. human-readable but not computer-readable, code):
+            // If number1 is greater than number2, return number1.  If number2 is the larger, return number2
+
+//            if (number1 > number2)
+//                return number1;
+//            else
+//            {
+//                Console.WriteLine("Whoops, got the else case!!!");
+//                return number2;
+//            }
+
+            // Shape of the if-statement (the "else <else-statement>" part is optional)
+//            if (<conditional>)
+//                <then-statement>
+//            else
+//                <else-statement>
+
+
+            // Style VERY VERY WRONG: omitting braces
+//            if (number1 > number2)
+//                return number1;
+//            else
+//                return number2;
+
+            // Style 1: each on its own line
+//            if (number1 > number2)
+//            {
+//                return number1;
+//            }
+//            else
+//            {
+//                return number2;
+//            }
+
+            // Style 2: save vertical lines, braces go on the same line as the control structures
+//            if (number1 > number2) {
+//                return number1;
+//            } else {
+//                return number2;
+//            }
+
+            // Because of the curly braces, adding the debugging statement does not break the control flow of the program.
+            // The return statement is still controlled by the if-statement.
+//            if (number1 == number2)
+//            {
+//                Console.WriteLine("Debugging here");
+//                return -1;
+//            }
+
+            // In this case, the "else" is necessary, so that we don't always run the "result = number2" line
+//            int result;
+//            if (number1 > number2)
+//            {
+//                result = number1;
+//            }
+//            else
+//            {
+//                result = number2;
+//            }
+//            return result;
+
+            // In this case, the "else" is not necessary, because we'll never run the "return number2" line if we've
+            // already run the "return number1" line (since "return" ends the execution of the method).
+//            if (number1 > number2)
+//            {
+//                return number1;
+//            }
+//            else
+//            {
+//                return number2;
+//            }
+
+//            if (number1 > number2)
+//            {
+//                return number1;
+//            }
+//            return number2;
+
+//            return (number1 > number2) ? number1 : number2;
+
+            return Math.Max(number1, number2);
+
+            // Side note: the following comment is now obsolete.  The current version of ReSharper no longer has this annoying behavior...
+            // ...when you type "Math.", it automatically includes the "using System.Math", and does not try to auto-correct "Math." to
+            // "DoingMath.".  Yay, ReSharper!
+            
             // Type "Math.", and look at the various mathematical functions that are defined for you.
             // Notice that you'll have to say "using System.Math" at the top, or type "System.Math.",
             // in order to see it.
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
