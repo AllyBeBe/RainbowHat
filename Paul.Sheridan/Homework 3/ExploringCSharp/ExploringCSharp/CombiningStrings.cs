eb3bf6430773879daaa9955718a82e6095aa7c70
Paul Sheridan <paul.sheridan@me.com>
2013-07-31 20:26:28 -0700

    Homework 3 Completed with extra credits

--- Paul.Sheridan/Homework 3/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
+++ Paul.Sheridan/Homework 3/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
@@ -12,14 +12,13 @@ namespace ExploringCSharp
         public string GreetsByCombiningStringsWithFormats(string name)
         {
             // try googling "string formatting in C#"
-            return null;
+            return string.Format("Hello, {0}", name);
         }
 
         public string GreetsByCombiningStringsWithStringBuilder(string name)
         {
             var builder = new StringBuilder(100);
             // Try typing "builder." and seeing what auto-complete options ReSharper gives you.
-            builder.
             return builder.ToString();
         }
     }
