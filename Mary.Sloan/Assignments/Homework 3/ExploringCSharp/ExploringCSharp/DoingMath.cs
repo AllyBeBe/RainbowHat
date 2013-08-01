6f59c730380e1a122571ae244a555efe3ef91721
Mary Sloan <M_hagen@Msn.com>
2013-08-01 14:15:26 -0700

    Homework 4 - All passing.
    Apparently, I need more practice at math.

--- Mary.Sloan/Assignments/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Mary.Sloan/Assignments/Homework 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -15,7 +15,7 @@ namespace ExploringCSharp
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            
+
             // typing math gave me a list of Math then DoingMath, so I didn't get the issue.
 
             return Math.Max(number1, number2);
@@ -38,12 +38,20 @@ namespace ExploringCSharp
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
 
-            // Well three out of seven - fails with 101 and all 10s.  Last math class I took around 20 years ago was community college algebra. And now I have a headache.
+            //I read other, more elegant, single line versions.  Here's how I think.  I know what the powers of 10 are.
+           
 
-            double x = Math.Log(number,10);
-            double y = Math.Pow(10, Math.Round(x));
-            double retval = number*y;
-            return (int) retval;
+            double i = 0;
+            while (number*(int)Math.Pow(10, i) < Int32.MaxValue)  // this could almost be anything that stays true, but this way the return value should still be an int.
+            {
+                if (number <= Math.Pow(10, i))
+                {
+                    return number*(int)Math.Pow(10, i);
+                }
+                i++;
+            }
+            return 0; //wouldn't compile without this.  "Not all code paths returned a value". I'd put some kind of error code here.
+
         }
     }
 }
