bd3b1ccd14644546dd8c8455128dbfd02fbae86c
Michael Gardiner <migard@uw.edu>
2013-08-01 14:30:40 -0700

    HW4 Challenge Mode Completed

---
+++ Michael.Gardiner/Homework 4/Calculator/CalculatorControllerTests/SimpleAdditionSubtractionTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorControllerTests
+{
+    public class SimpleAdditionSubtractionTests
+    {
+        private readonly CalculatorController _controller = new CalculatorController();
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _controller.Clear();
+        }
+
+        [Test]
+        public void CanPerformSimpleAddition()
+        {
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('+');
+            _controller.AcceptCharacter('1');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void CanPerformSimpleSubtraction()
+        {
+            _controller.AcceptCharacter('6');
+            _controller.AcceptCharacter('-');
+            _controller.AcceptCharacter('2');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void CanPerformMutilpeDigitAddition()
+        {
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('6');
+            _controller.AcceptCharacter('+');
+            _controller.AcceptCharacter('4');
+            _controller.AcceptCharacter('2');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("58"));
+        }
+
+        [Test]
+        public void CanPerformMutipleDigitSubtraction()
+        {
+            _controller.AcceptCharacter('2');
+            _controller.AcceptCharacter('2');
+            _controller.AcceptCharacter('-');
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('2');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("10"));
+        }
+    }
+}
---
+++ Michael.Gardiner/Homework 4/Calculator/CalculatorControllerTests/SimpleMultiplicationDevisionTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorControllerTests
+{
+    internal class SimpleMultiplicationDevisionTests
+    {
+        private readonly CalculatorController _controller = new CalculatorController();
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _controller.Clear();
+        }
+
+        [Test]
+        public void CanPerformSimpleMultiplication()
+        {
+            _controller.AcceptCharacter('5');
+            _controller.AcceptCharacter('*');
+            _controller.AcceptCharacter('6');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("30"));
+        }
+
+        [Test]
+        public void CanPerformSimpleDivision()
+        {
+            _controller.AcceptCharacter('2');
+            _controller.AcceptCharacter('4');
+            _controller.AcceptCharacter('/');
+            _controller.AcceptCharacter('3');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("8"));
+        }
+
+        [Test]
+        public void CanPerformMutilpeDigitMultiplication()
+        {
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('6');
+            _controller.AcceptCharacter('x');
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('2');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("192"));
+        }
+
+        [Test]
+        public void CanPerformMutipleDigitDivision()
+        {
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('4');
+            _controller.AcceptCharacter('4');
+            _controller.AcceptCharacter('/');
+            _controller.AcceptCharacter('1');
+            _controller.AcceptCharacter('2');
+
+            Assert.That(_controller.GetOutput(), Is.EqualTo("12"));
+        }
+    }
+}
