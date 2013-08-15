24b27961ccb96e37e9ec64c1745be2f6a673b2b4
Gordon Hew <gordonhew@gmail.com>
2013-08-15 01:49:46 -0400

    Homework 6 first attempt

--- Gordon.Hew/Homework 6/Calculator/Calculator/CalculatorController.cs
+++ Gordon.Hew/Homework 6/Calculator/Calculator/CalculatorController.cs
@@ -1,18 +1,96 @@
-﻿namespace Calculator
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Calculator
 {
     // NOTE: this class has to be marked with "public" so that it is visible to the CalculatorControllerTests project.
     public class CalculatorController
     {
-        private int _currentValue;
+        private char _operator;
+
+        private String _inputCurrent;
+
+        private String _inputPrevious;
+
+        private String _outputValue;
+
+        public CalculatorController()
+        {
+            _operator = ' ';
+            _inputCurrent = "";
+            _inputPrevious = "";
+            _outputValue = "0";
+        }
 
         // This method is the core method of CalculatorController.  In Homework 5, when you are making
         // the tests we co-create in Homework 4 pass, you'll write code in this method (and probably in
         // helper methods that it calls) to make the calculator behave according to the tests.
         public void AcceptCharacter(char input)
         {
-            // Your code will eventually go here, to make all of the tests pass.
+            if ('c' == input)
+            {
+                _inputCurrent = "";
+                _outputValue = "0";
+                _operator = ' ';
+            }
+            else if (Char.IsDigit(input) || input == '.' || (String.Empty.Equals(_inputCurrent) && input == '-'))
+            {
+                _inputCurrent += input;
+                _outputValue = _inputCurrent;                
+            }
+            else if ('=' == input)
+            {
+                decimal result = 0;
+                decimal lastInputValue = Convert.ToDecimal(_inputPrevious);
+                decimal currentInputValue = Convert.ToDecimal(_inputCurrent);
 
-            // DO NOT WRITE THIS CODE YET!  WRITING THIS CODE WILL BE HOMEWORK 5!
+                switch (_operator)
+                {
+                    case '+':
+                    {
+                        result = lastInputValue + currentInputValue;
+                        break;
+                    }
+                    case '-':
+                    {
+                        result = lastInputValue - currentInputValue;
+                        break;
+                    }
+                    case '/':
+                    {
+                        result = lastInputValue/currentInputValue;
+                        break;
+                    }
+                    case '*':
+                    {
+                        result = lastInputValue*currentInputValue;
+                        break;
+                    }
+                    default:
+                    {
+                        result = lastInputValue;
+                        break;
+                    }
+                }
+
+                Console.WriteLine(lastInputValue + " " + _operator + " " + currentInputValue + " = " + result);
+
+                _outputValue = result.ToString();
+                _inputPrevious = result.ToString();
+
+                Console.WriteLine(lastInputValue + " " + _operator + " " + currentInputValue + " = " + _inputPrevious);
+                
+                _operator = ' ';
+
+
+            }
+            else
+            {
+                _operator = input;
+                _inputPrevious = _inputCurrent;
+                _inputCurrent = "";
+            }
         }
 
         // Someday, this method will return the string that should be displayed in the "output window" of the 
@@ -20,7 +98,10 @@
         // return something.
         public string GetOutput()
         {
-            return "13";
+            if("-".Equals(_outputValue))
+                return "0";
+
+            return _outputValue;
         }
     }
 }
