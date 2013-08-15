5280529f267fedc30988b9159b68a71981bbc99a
Amy Hurlow <aehurlow@gmail.com>
2013-08-14 22:08:05 -0700

    Hw 6: images of attempt to visualize the calc software

--- Amy.Hurlow/Homework 6 (2nd try)/Calculator/Calculator/CalculatorController.cs
+++ Amy.Hurlow/Homework 6 (2nd try)/Calculator/Calculator/CalculatorController.cs
@@ -4,7 +4,25 @@
     public class CalculatorController
     {
         private int _currentValue;
-
+        // This is the first test I'm trying to make pass.
+        // I'm having a very hard time understanding the structure of the code I'll need to write.
+        // I immediately started drawing boxes on paper, but got lost trying to relate display behavior 
+        // and math operators.
+        // I
+        // I need some way to visualize the variables and methods I'll need, and their relation to each other.
+        // Unfortunately, I Googled "how to (plan/design) software" and  
+        // my search led me to discover oh so many systems for modelling, each of which
+        // would take time to learn.  The diagrams and structures did not seem intuitive, and seemed to require
+        // actually understanding software architecture.
+        // 
+        // So I'll think aloud here, and see if I can come up with ways to make this one test work,
+        // without trying to think about the rest of the program.
+        // 
+        // I need variables in which to store each digit and operator entered, so I can combine them at "=".
+        // But I don't know how many digits or operators the user will enter.
+        // I can clear the variables and re-use them on the next go-round when the next button 
+        // is clicked after the user clicks "=" and I show the answer
+        private 
         // This method is the core method of CalculatorController.  In Homework 5, when you are making
         // the tests we co-create in Homework 4 pass, you'll write code in this method (and probably in
         // helper methods that it calls) to make the calculator behave according to the tests.
