fc1d96e359b07e6dbe8d9f5cca4927a1de182e05
Gordon Hew <gordonhew@gmail.com>
2013-08-16 00:00:59 -0400

    Homework 6 - 51/77 Tests Passed
    Unary, multiple operations, and unary tests still not passing

--- Gordon.Hew/Homework 6/Calculator/Calculator/CalculatorController.cs
+++ Gordon.Hew/Homework 6/Calculator/Calculator/CalculatorController.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Calculator
 {
     // NOTE: this class has to be marked with "public" so that it is visible to the CalculatorControllerTests project.
     public class CalculatorController
     {
+        private const string ZeroString = "0";
+
+        private const string DivideNanMessage = "Result is undefined";
+
+        private const string DivideInfinityMessage = "Cannot divide by zero";
+        
         private char _operator;
+
+        /*TODO: these should be domain objects, oh well... */
 
         private String _inputCurrent;
 
+        private double _inputCurrentDouble;
+
         private String _inputPrevious;
+
+        private double _inputPreviousDouble;
 
         private String _outputValue;
 
+        private int _inputCurrentDigitCounter;
+
         public CalculatorController()
         {
+            Init();
+        }
+
+        private void Init()
+        {
             _operator = ' ';
-            _inputCurrent = "";
-            _inputPrevious = "";
-            _outputValue = "0";
+            _inputCurrent = String.Empty;
+            _inputCurrentDouble = 0;
+            _inputPrevious = String.Empty;
+            _inputPreviousDouble = 0;
+            _outputValue = ZeroString;
+            _inputCurrentDigitCounter = 0;
         }
 
         // This method is the core method of CalculatorController.  In Homework 5, when you are making
@@ -30,78 +53,120 @@ namespace Calculator
         {
             if ('c' == input)
             {
-                _inputCurrent = "";
-                _outputValue = "0";
-                _operator = ' ';
+                Init();
             }
-            else if (Char.IsDigit(input) || input == '.' || (String.Empty.Equals(_inputCurrent) && input == '-'))
+            else if (IsValidInputNumber(input))
             {
-                _inputCurrent += input;
-                _outputValue = _inputCurrent;                
+                /* Ignore attempts to add leading zeroes */
+                if (!IsAddingLeadingZero(input))
+                {
+                    /* Replace value wholesale if previous value is zero */
+                    if (IsReplaceZero(input))
+                    {
+                        _inputCurrent = input.ToString(CultureInfo.InvariantCulture);
+                        _outputValue = _inputCurrent;
+                    }
+                    else
+                    {
+                        if (_inputCurrentDigitCounter < 15)
+                        {
+                            _inputCurrent += input;
+                            _outputValue = _inputCurrent;
+
+                            if (Char.IsDigit(input))
+                                _inputCurrentDigitCounter++;
+                        }
+                    }
+                }
             }
             else if ('=' == input)
             {
-                decimal result = 0;
-                decimal lastInputValue = Convert.ToDecimal(_inputPrevious);
-                decimal currentInputValue = Convert.ToDecimal(_inputCurrent);
+                _inputCurrentDouble = Double.Parse(_inputCurrent);
+
+                double result = 0;
 
                 switch (_operator)
                 {
                     case '+':
                     {
-                        result = lastInputValue + currentInputValue;
+                        result = _inputPreviousDouble + _inputCurrentDouble;
+                        _outputValue = result.ToString("G");
                         break;
                     }
                     case '-':
                     {
-                        result = lastInputValue - currentInputValue;
+                        result = _inputPreviousDouble - _inputCurrentDouble;
+                        _outputValue = result.ToString("G");
                         break;
                     }
                     case '/':
                     {
-                        result = lastInputValue/currentInputValue;
+                        result = _inputPreviousDouble / _inputCurrentDouble;
+
+                        if (Double.IsNaN(result))
+                            _outputValue = DivideNanMessage;
+                        else if (Double.IsInfinity(result))
+                            _outputValue = DivideInfinityMessage;
+                        else
+                            _outputValue = result.ToString("G");
+
                         break;
                     }
                     case '*':
                     {
-                        result = lastInputValue*currentInputValue;
+                        result = _inputPreviousDouble * _inputCurrentDouble;
+                        _outputValue = result.ToString("G");
                         break;
                     }
                     default:
                     {
-                        result = lastInputValue;
+                        result = _inputCurrentDouble;
+                        _outputValue = result.ToString("G");
                         break;
                     }
                 }
 
-                Console.WriteLine(lastInputValue + " " + _operator + " " + currentInputValue + " = " + result);
-
-                _outputValue = result.ToString();
-                _inputPrevious = result.ToString();
-
-                Console.WriteLine(lastInputValue + " " + _operator + " " + currentInputValue + " = " + _inputPrevious);
-                
+                Console.WriteLine(_inputPreviousDouble.ToString("G") + " " + _operator + " " + _inputCurrentDouble.ToString("G") + "=" + result.ToString("G"));
+               
+                _inputPrevious = result.ToString("G");
+                _inputCurrentDigitCounter = 0;
                 _operator = ' ';
-
-
             }
             else
             {
                 _operator = input;
                 _inputPrevious = _inputCurrent;
-                _inputCurrent = "";
+                _inputPreviousDouble = Double.Parse(_inputPrevious);
+                _inputCurrent = String.Empty;
+                _inputCurrentDouble = 0;
+                _inputCurrentDigitCounter = 0;
             }
         }
 
-        // Someday, this method will return the string that should be displayed in the "output window" of the 
-        // calculator.  For now, it just returns a dummy value of "13", since the compiler requires that it
-        // return something.
         public string GetOutput()
         {
-            if("-".Equals(_outputValue))
-                return "0";
-
             return _outputValue;
+        }
+
+        private Boolean IsValidInputNumber(char input)
+        {
+            /* Checks if the value is a digit, decimal point, or the start of a negative number */
+            return Char.IsDigit(input) || input == '.' || String.Empty.Equals(_inputCurrent) && input == '-'; ;
+        }
+
+        private Boolean IsAddingLeadingZero(char input)
+        {
+            return input == '0' && _inputCurrent.Equals(ZeroString);
+        }
+
+        private Boolean IsReplaceZero(char input)
+        {
+            return ZeroString.Equals(_inputCurrent);
+        }
+
+        private Boolean IsUnaryOperation(char input)
+        {
+            return false;
         }
     }
 }
