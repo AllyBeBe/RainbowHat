05f218276070a382a2d8da52ebd8e63bd8b0733a
Mickey Phoenix <meowse@remove.gmail.remove.com>
2013-08-19 13:08:24 -0700

    Office hours Su's questions Su's code

--- Instructor/Sessions/Office Hours 7.5 (a)/Su's Questions/Homework 6/Calculator/Calculator/CalculatorController.cs
+++ Instructor/Sessions/Office Hours 7.5 (a)/Su's Questions/Homework 6/Calculator/Calculator/CalculatorController.cs
@@ -1,15 +1,72 @@
-﻿namespace Calculator
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Calculator
 {
     // NOTE: this class has to be marked with "public" so that it is visible to the CalculatorControllerTests project.
     public class CalculatorController
     {
-        private int _currentValue;
+      
+        private char _operator;
+
+        private string _currentValue;
+        private string _previousValue;
+        private string _result;
+        
+
+        public CalculatorController()
+
+        {
+            _operator = '0';
+            _currentValue = "";
+            _previousValue = "";
+            _result = "";
+        }
+
 
         // This method is the core method of CalculatorController.  In Homework 5, when you are making
         // the tests we co-create in Homework 4 pass, you'll write code in this method (and probably in
         // helper methods that it calls) to make the calculator behave according to the tests.
         public void AcceptCharacter(char input)
         {
+            if (input == 'c')
+            {
+                _currentValue = "";
+            }
+
+            else if (_currentValue == "")
+            {
+                _currentValue = input.ToString();
+                _result = "";
+
+            }
+
+            else if (input == '+')
+            {
+                _operator = input;
+            }
+
+            else if (_currentValue != "" && input != '=')
+            {
+                _previousValue = _currentValue;
+                _currentValue = input.ToString();
+            }
+
+            else if (input == '=')
+            {
+                if (_operator == '+')
+                {
+                    _result = (Convert.ToInt16(_previousValue) + Convert.ToInt16(_currentValue)).ToString();
+                }
+            }
+
+            //else
+            //{
+
+            //    _currentStringValue = _currentStringValue + input;
+            //}
+
             // Your code will eventually go here, to make all of the tests pass.
 
             // DO NOT WRITE THIS CODE YET!  WRITING THIS CODE WILL BE HOMEWORK 5!
@@ -20,7 +77,11 @@
         // return something.
         public string GetOutput()
         {
-            return "13";
+            if (_result != "")
+            {
+                return _result;
+            }
+            return _currentValue;
         }
     }
 }
