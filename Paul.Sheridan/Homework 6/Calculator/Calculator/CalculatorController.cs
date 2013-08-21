11b6de758bf22c6b9720fec8d3d810c2bc3fb84f
Paul Sheridan <paul.sheridan@me.com>
2013-08-20 18:26:11 -0700

    More small changes.
    63 tests passing. Divide by zero implemented.  Saved number resolved for
    the most part.

--- Paul.Sheridan/Homework 6/Calculator/Calculator/CalculatorController.cs
+++ Paul.Sheridan/Homework 6/Calculator/Calculator/CalculatorController.cs
@@ -15,93 +15,87 @@ namespace Calculator
         private string _operator;
         private bool _isWaitingForSecondOperand;
         private bool _equalsWasJustPressed;
+        private bool _isDivideZeroByZero;
+        private bool _isDivideNumberByZero;
 
         public CalculatorController()
         {
             ResetCalculatorState();
-        }
-
-        // This method is the core method of CalculatorController.  In Homework 5, when you are making
-        // the tests we co-create in Homework 4 pass, you'll write code in this method (and probably in
-        // helper methods that it calls) to make the calculator behave according to the tests.
+        } 
 
         public void AcceptCharacter(char input)
         {
-            if (input == 'c')
+            switch (input)
             {
-                ResetCalculatorState();
-            }
-            else if (input == '+')
-            {
-                _previousValue = _currentValue;
-                _currentValue = 0;
-                _operator = "+";
-                _isWaitingForSecondOperand = true;
-                _equalsWasJustPressed = false;
-            }
-            else if (input == '-')
-            {
-                // If Operator is not null (we already have an operator entered), 
-                // execute the math operation represented by Operator (the saved operator).
-                // Otherwise, just copy _currentValue into 
-                // _previousValue, and set _currentValue to 0
-                if (_operator != null)
-                {
-                    DoMathWithSavedOperator();
-                }
-                else
-                {
+                case 'c':
+                    ResetCalculatorState();
+                    break;
+                case '+':
+                    if (_operator != null)
+                    {
+                        DoMathWithSavedOperator();
+                    }
                     _previousValue = _currentValue;
-                    _currentValue = 0;                    
-                }
-                _operator = "-";
-                _isWaitingForSecondOperand = true;
-                _equalsWasJustPressed = false;
-            }
-            else if (input == '*')
-            {
-                _previousValue = _currentValue;
-                _currentValue = 0;
-                _operator = "*";
-                _isWaitingForSecondOperand = true;
-                _equalsWasJustPressed = false;
-            }
-            else if (input == '/')
-            {
-                _previousValue = _currentValue;
-                _currentValue = 0;
-                _operator = "/";
-                _isWaitingForSecondOperand = true;
-                _equalsWasJustPressed = false;
-            }
-            else if (input == '=')
-            {
-                DoMathWithSavedOperator();
-                _isWaitingForSecondOperand = false;
-                _previousValue = _currentValue;
-                _equalsWasJustPressed = true;
-            }
-            else // Digit has been typed
-            {
-                // If we were waiting for a second operand, we're not
-                // anymore -- we've started getting the second operand.
-                // If we weren't waiting for a second operand, this line
-                // has no effect (and is harmless).
-                _isWaitingForSecondOperand = false;
-
-                if (_currentValue.ToString().Length < 15)
-                {
-                    if (_equalsWasJustPressed == false)
-                    {
-                        _currentValue = _currentValue*10 + int.Parse(input.ToString());
-                    }
-                    else
-                    {
-                        _currentValue = int.Parse(input.ToString());
-                        _previousValue = _currentValue;
-                    }
+                    _currentValue = 0;
+                    _operator = "+";
+                    _isWaitingForSecondOperand = true;
                     _equalsWasJustPressed = false;
-                }
+                    break;
+                case '-':
+                    if (_operator != null)
+                    {
+                        DoMathWithSavedOperator();
+                    }
+                    _operator = "-";
+                    _isWaitingForSecondOperand = true;
+                    _equalsWasJustPressed = false;
+                    _previousValue = _currentValue;
+                     _currentValue = 0;
+                    break;
+                case '*':
+                    if (_operator != null)
+                    {
+                        DoMathWithSavedOperator();
+                    }                   
+                    _previousValue = _currentValue;
+                    _currentValue = 0;
+                    _operator = "*";
+                    _isWaitingForSecondOperand = true;
+                    _equalsWasJustPressed = false;
+                    break;
+                case '/':
+                    if (_operator != null)
+                    {
+                        DoMathWithSavedOperator();
+                    }
+                    _previousValue = _currentValue;
+                    _currentValue = 0;
+                    _operator = "/";
+                    _isWaitingForSecondOperand = true;
+                    _equalsWasJustPressed = false;
+                    break;
+                case '=':
+                    DoMathWithSavedOperator();
+                    _isWaitingForSecondOperand = false;
+                    _previousValue = _currentValue;
+                    _equalsWasJustPressed = true;
+                    break;
+                default:
+                    _isWaitingForSecondOperand = false;
+                    _equalsWasJustPressed = false;
+                    if (_currentValue.ToString().Length < 15)
+                    {
+                        if (_equalsWasJustPressed == false)
+                        {
+                            _currentValue = _currentValue*10 + int.Parse(input.ToString());
+                        }
+                        else
+                        {
+                            _currentValue = int.Parse(input.ToString());
+                            _previousValue = _currentValue;
+                        }                        
+                    }
+                    break;
             }
         }
 
@@ -114,19 +108,12 @@ namespace Calculator
             _equalsWasJustPressed = false;
         }
 
-        // (alternate approach, have a lookup table from the
-        // operator character to a function that does the
-        // appropriate math)
         private void DoMathWithSavedOperator()
         {
-            // Take saved values _previousValue and _currentValue
-            // and if the operator was +, add them,
             if (_operator == "+")
             { 
                 _currentValue = _previousValue + _currentValue;
-                _previousValue = 0;
             }
-            // if the operator was -, subtract them, 
             if (_operator == "-")
             {
                 _currentValue = _previousValue - _currentValue;
@@ -141,11 +128,11 @@ namespace Calculator
                 {
                     if (_previousValue == 0)
                     {
-                        ;
+                        _isDivideZeroByZero = true;
                     }
                     else
                     {
-                        ;
+                        _isDivideNumberByZero = true;
                     }
                 }
                 else
@@ -159,6 +146,14 @@ namespace Calculator
         {
             // If we are waiting for a second operand, display
             // _previousValue rather than _currentValue
+            if (_isDivideNumberByZero)
+            {
+                return "Result is undefined";
+            }
+            if (_isDivideZeroByZero)
+            {
+                return "Result is undefined";
+            }
             if (_isWaitingForSecondOperand)
             {
                 return _previousValue.ToString();
