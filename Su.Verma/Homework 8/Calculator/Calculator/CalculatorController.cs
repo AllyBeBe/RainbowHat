fdaaef5837482c8bba694d9713b7e71af64416f4
Su Verma <sugandha.verma7@gmail.com>
2013-08-28 23:25:14 -0700

    HW8 - 56 Tests passing

--- Su.Verma/Homework 8/Calculator/Calculator/CalculatorController.cs
+++ Su.Verma/Homework 8/Calculator/Calculator/CalculatorController.cs
@@ -18,7 +18,7 @@ namespace Calculator
         private string _result;
         private bool _IsFirstOperatorBeforeNumber;
         private bool _IsFirstOperatorAddedToCurrentValue;
-        private static bool _IsWaitingForSecondOperand;
+        private static bool _IsFirstNumberEntered;
         
         private bool _IsTryingToDivideNonZeroByZero;
         private bool _IsTryingToDivideZeroByZero;
@@ -36,6 +36,8 @@ namespace Calculator
             _currentValue = "0";
             _previousValue = "";
             _result = "";
+            _IsFirstNumberEntered = false;
+            _IsFirstOperatorBeforeNumber = false;
         }
 
 
@@ -51,9 +53,9 @@ namespace Calculator
             }
 
                 // Record the '+' operator or '-' operator or '*' operator or '/' operator
-            else if (input == '+' || input  == '-' || input == '*' || input == '/')
+            else if (input == '+' || input == '-' || input == '*' || input == '/')
             {
-                if (_currentValue == "0" && input != '+')
+                if (_currentValue == "0" && input != '+' && _IsFirstNumberEntered != true)
                 {
                     _IsFirstOperatorBeforeNumber = true;
                     _operator = input;
@@ -64,29 +66,32 @@ namespace Calculator
                 {
                     _currentValue = _operator + _currentValue;
                 }
-                
+
                 _operator = input;
-                    // save the '_currentValue' into '_previousValue'
-                    _previousValue = _currentValue;
-                    // Clear out the _currentvalue
-                    _currentValue = "";
-                    _IsWaitingForSecondOperand = true;
-                    _IsFirstOperatorBeforeNumber = false;
+                // save the '_currentValue' into '_previousValue'
+                _previousValue = _currentValue;
+                // Clear out the _currentvalue
+                _currentValue = "";
                 
+                _IsFirstOperatorBeforeNumber = false;
+                _IsFirstNumberEntered = false;
             }
 
             else if (input == '=')
             {
                 if (_IsFirstOperatorBeforeNumber == true)
                 {
-                    if (_operator == '/')
+                    if (_currentValue != "0")
                     {
-                        _result = "0";
-                        return;
+                        if (_operator == '/' || _operator == '*')
+                        {
+                            _result = "0";
+                            return;
+                        }
+                        _result = _operator + _currentValue;
                     }
-                    _result = _operator + _currentValue;
                 }
-                else  if (_operator == '+')
+                else if (_operator == '+')
                 {
                     _result = Convert.ToString(Convert.ToDouble(_previousValue) + (Convert.ToDouble(_currentValue)));
                 }
@@ -96,32 +101,35 @@ namespace Calculator
                 }
                 else if (_operator == '*')
                 {
-                    _result = Convert.ToString(Convert.ToDouble(_previousValue) * (Convert.ToDouble(_currentValue)));
+                    _result = Convert.ToString(Convert.ToDouble(_previousValue)*(Convert.ToDouble(_currentValue)));
                 }
                 else if (_operator == '/')
-                //{
-                //    if (_currentValue == "0")
-                //    {
-                //        if (_previousValue == "0")
-                //        {
-                //            _IsTryingToDivideNonZeroByZero = true;
-                //        }
-                //        else
-                //        {
-                //            _IsTryingToDivideZeroByZero = true;
-                //        }
-                //    }
-                //}
-                //else if
                 {
-                    _result = Convert.ToString(Convert.ToDouble(_previousValue) / (Convert.ToDouble(_currentValue)));
+                    if (_currentValue == "0")
+                    {
+                        if (_previousValue == "0")
+                        {
+                            _IsTryingToDivideNonZeroByZero = false;
+                            _IsTryingToDivideZeroByZero = true;
+
+                        }
+                        else
+                        {
+                            _IsTryingToDivideNonZeroByZero = true;
+                            _IsTryingToDivideZeroByZero = false;
+                        }
+                    }
+
+                    else
+                    {
+                    _result = Convert.ToString(Convert.ToDouble(_previousValue)/(Convert.ToDouble(_currentValue)));
+                    }
+
+                    //_result = _currentValue;
+                    //_currentValue = "";
+
                 }
-
-                //_result = _currentValue;
-                //_currentValue = "";
-
             }
-                //There is no _currentValue and user has typed a digit
 
             else if (_currentValue == "")
             {
@@ -141,26 +149,16 @@ namespace Calculator
 
                 //Only add the digits if we have fewer than 15 digits entered 
 
-                  if (_currentValue.Length < 15)
-                    {
-                        _currentValue = _currentValue + input;
-                    }
+                if (_currentValue.Length < 15)
+                {
+                    _currentValue = _currentValue + input;
+                    _IsFirstNumberEntered = true;
+                }
 
             }
 
-
-
-
-            //else
-            //{
-
-            //    _currentStringValue = _currentStringValue + input;
-            //}
-
-            // Your code will eventually go here, to make all of the tests pass.
-
-            // DO NOT WRITE THIS CODE YET!  WRITING THIS CODE WILL BE HOMEWORK 5!
         }
+        
 
         // Someday, this method will return the string that should be displayed in the "output window" of the 
         // calculator.  For now, it just returns a dummy value of "13", since the compiler requires that it
@@ -169,17 +167,17 @@ namespace Calculator
         {
             // if we'r trying to Divide anything else by Zero
             // return "Cannot Divide by Zero"
-           //// if (_IsTryingToDivideNonZeroByZero)
-           // {
-           //     return "Cannot Divide by Zero";
-           // }
+            if (_IsTryingToDivideNonZeroByZero)
+            {
+                return "Cannot divide by zero";
+            }
 
-            // if we'r trying to Divide Zero By Zero
+             // if we'r trying to Divide Zero By Zero
             // return "Result is undefined"
-            //if (_IsTryingToDivideZeroByZero)
-            //{
-            //    return "Result is undefined";
-            //}
+           if (_IsTryingToDivideZeroByZero)
+            {
+                return "Result is undefined";
+            }
             if (_result != "")
             {
                 return _result;
@@ -206,9 +204,7 @@ namespace Calculator
 
      ///   return _currentValue.ToString();
   //  }
-
-
-
+        
     }
 
 }
