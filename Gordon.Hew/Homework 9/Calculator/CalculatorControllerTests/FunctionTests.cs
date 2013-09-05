4833fbd7a3ec3d72e3563f32c48c978c619acfe6
Gordon Hew <gordonhew@gmail.com>
2013-09-05 00:05:54 -0400

    Homework 9 Added some unit tests
    Added unit tests to test new functions (i.e. tan, sine, reciprocal,
    etc.)
    
    Not sure how the behavior for % works in the Dirk Gently web calculator,
    doesn't seem to match a normal calculator.

---
+++ Gordon.Hew/Homework 9/Calculator/CalculatorControllerTests/FunctionTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CalculatorControllerTests
+{
+    class FunctionTests : BaseTestFixture
+    {
+        [Test]
+        public void CanTanZeroRadians()
+        {
+            ForInputExpect("0t", "0");
+        }
+        [Test]
+        public void CanTanFortyFiveRadians()
+        {
+            ForInputExpect("45t", "1.61977519054386");
+        }
+
+        [Test]
+        public void CanCosineZeroRadians()
+        {
+            ForInputExpect("0o", "1");
+        }
+
+        [Test]
+        public void CanCosineNinetyRadians()
+        {
+            ForInputExpect("90o", "-0.44807361612917");
+        }
+
+        [Test]
+        public void CanSineZeroRadians()
+        {
+            ForInputExpect("0s", "0");
+        }
+
+        [Test]
+        public void CanSineNinetyRadians()
+        {
+            ForInputExpect("90s", "0.893996663600558");
+        }
+
+        [Test]
+        public void CanSquarePositive()
+        {
+            ForInputExpect("3q", "9");
+        }
+
+        [Test]
+        public void CanSquareNegative()
+        {
+            ForInputExpect("-3q", "9");
+        }
+
+        [Test]
+        public void CanNegatePositive()
+        {
+            ForInputExpect("1#", "-1");
+        }
+
+        [Test]
+        public void CanNegateNegative()
+        {
+            ForInputExpect("-1#", "1");
+        }
+
+        [Test]
+        public void CanReciprocal()
+        {
+            ForInputExpect("10r", "0.1");
+        }
+    }
+}
