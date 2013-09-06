6a0e3c0264d52d675c3baa4aba67d2c335168b4f
Paul Sheridan <paul.sheridan@me.com>
2013-09-05 21:02:28 -0700

    Session 10 code w/ parking lot

---
+++ Instructor/Sessions/Session 10/PropertiesExamples/PropertiesExamples/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PropertiesExamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WholeNameLabel = new System.Windows.Forms.Label();
+            this.AlphabetizedNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WholeNameLabel
+            // 
+            this.WholeNameLabel.AutoSize = true;
+            this.WholeNameLabel.Location = new System.Drawing.Point(13, 13);
+            this.WholeNameLabel.Name = "WholeNameLabel";
+            this.WholeNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.WholeNameLabel.TabIndex = 0;
+            this.WholeNameLabel.Text = "label1";
+            // 
+            // AlphabetizedNameLabel
+            // 
+            this.AlphabetizedNameLabel.AutoSize = true;
+            this.AlphabetizedNameLabel.Location = new System.Drawing.Point(16, 58);
+            this.AlphabetizedNameLabel.Name = "AlphabetizedNameLabel";
+            this.AlphabetizedNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.AlphabetizedNameLabel.TabIndex = 1;
+            this.AlphabetizedNameLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.AlphabetizedNameLabel);
+            this.Controls.Add(this.WholeNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WholeNameLabel;
+        private System.Windows.Forms.Label AlphabetizedNameLabel;
+    }
+}
+
---
+++ Instructor/Sessions/Session 10/PropertiesExamples/PropertiesExamples/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertiesExamples
+{
+    public partial class Form1 : Form
+    {
+        // The "backing field" (the private variable that actually holds the value)
+        // for the property FirstName
+        private string _firstName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            private set
+            {
+                _firstName = value;
+            }
+        }
+
+        // This is an "autoprop".  It is exactly the same (for LastName) as the above
+        // 12 lines are for FirstName, but it's only one line.
+        // Autoprops let you control access to the getter and the setter independently,
+        // without the hassle of 12 lines of boiler-plate.
+        public string LastName { get; private set; }
+
+        // Syntax for a property -- has curly braces instead of a semicolon,
+        // and has either a "get" method or a "set" method or both.
+        public string WholeName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }    
+        }
+
+        public string AlphabetizedName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+            set
+            {
+                string[] nameParts = value.Split(new [] {','});
+                LastName = nameParts[0].Trim();
+                FirstName = nameParts[1].Trim();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            FirstName = "Jonathan";
+            LastName = "Seagull";
+
+            AlphabetizedName = "Goodall, Jane";
+
+            WholeNameLabel.Text = WholeName;
+            //AlphabetizedNameLabel.Text = LastName + ", " + FirstName;
+            AlphabetizedNameLabel.Text = AlphabetizedName;
+        }
+    }
+}
